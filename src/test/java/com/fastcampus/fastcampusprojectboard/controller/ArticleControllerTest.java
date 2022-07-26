3b8afc0a25b0b904910b2f1b7192c6bcd3e06b8e
wongyun park <dlsrk489@gmail.com>
2022-07-26 21:41:25 +0900

    #18 - 게시판 뷰 기본 골격 작성
    컨트롤러 핸드러 메소드로 테스트를 통과하게 열어줌 
    테스트에서는 media type 검사에서 부가적인 옵션이 들어갈 수 있음을 확인 했기에 테스트 메소드 수정

--- src/test/java/com/fastcampus/fastcampusprojectboard/controller/ArticleControllerTest.java
+++ src/test/java/com/fastcampus/fastcampusprojectboard/controller/ArticleControllerTest.java
@@ -23,7 +23,7 @@ class ArticleControllerTest {
         this.mvc = mvc;
     }
 
-    @Disabled("구현 중")
+//    @Disabled("구현 중")
     @DisplayName("[view][GET] 게시글 리스트 (게시판) 페이지 - 정상 호출")
     @Test
     public void givenNothing_whenRequestingArticlesView_thenReturnArticleㄴView() throws Exception {
@@ -32,7 +32,7 @@ class ArticleControllerTest {
            // When & Then
         mvc.perform(get("/articles"))
                 .andExpect(status().isOk()) // 상태 반환 값 검사
-                .andExpect(content().contentType(MediaType.TEXT_HTML)) // 미디어 타입 검사
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML)) // 미디어 타입 검사
                 .andExpect(view().name("articles/index")) // view name 검사
                 .andExpect(model().attributeExists("articles")); // modelAttribute 맵에 articles라는 키가 있는지 검사
     }
